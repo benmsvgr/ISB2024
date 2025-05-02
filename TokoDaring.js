586781da3870ed6732c509d8115684962127e35b
benmsvgr <62933415+benmsvgr@users.noreply.github.com>
2025-05-02 12:58:50 +0700

    Update TokoDaring.js

--- TokoDaring.js
+++ TokoDaring.js
@@ -16,8 +16,8 @@ const url = process.env.API_URL_TD; // Use environment variable for URL
 
 // Define the spreadsheet ID and range
 const spreadsheetId = process.env.SPREADSHEET_ID; // Use environment variable for Spreadsheet ID
-const clearRange = 'TokoDaring!A:ZZ'; // Range to clear
-const updateRange = 'TokoDaring!A1'; // Range to update
+const clearRange = 'M-Program!A:ZZ'; // Range to clear
+const updateRange = 'M-Program!A1'; // Range to update
 
 async function fetchData() {
     try {
