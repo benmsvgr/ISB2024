df1ac8ed72cbe738948c4681fbc0f63f22265167
benmsvgr <62933415+benmsvgr@users.noreply.github.com>
2025-05-02 12:57:44 +0700

    Update Swakelola_Terumumkan.js

--- Swakelola_Terumumkan.js
+++ Swakelola_Terumumkan.js
@@ -16,8 +16,8 @@ const url = process.env.API_URL_SWA; // Use environment variable for URL
 
 // Define the spreadsheet ID and range
 const spreadsheetId = process.env.SPREADSHEET_ID; // Use environment variable for Spreadsheet ID
-const clearRange = 'tes1!A:ZZ'; // Range to clear
-const updateRange = 'tes2!A1'; // Range to update
+const clearRange = 'M-Kegiatan!A:ZZ'; // Range to clear
+const updateRange = 'M-Kegiatan!A1'; // Range to update
 
 async function fetchData() {
     try {
