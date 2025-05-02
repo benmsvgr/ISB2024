96bf2f85d23c33a2e387368fc63f760e8cf5347a
benmsvgr <62933415+benmsvgr@users.noreply.github.com>
2025-05-02 13:16:08 +0700

    Update Tender_Selesai_Pengumuman.js

--- Tender_Selesai_Pengumuman.js
+++ Tender_Selesai_Pengumuman.js
@@ -16,8 +16,8 @@ const url = process.env.API_URL_TSP; // Use environment variable for URL
 
 // Define the spreadsheet ID and range
 const spreadsheetId = process.env.SPREADSHEET_ID; // Use environment variable for Spreadsheet ID
-const clearRange = 'RUP-PaketAnggaranPenyedia!A:ZZ'; // Range to clear
-const updateRange = 'RUP-PaketAnggaranPenyedia!A1'; // Range to update
+const clearRange = 'M-SubKegiatan!A:ZZ'; // Range to clear
+const updateRange = 'M-SubKegiatan!A1'; // Range to update
 
 async function fetchData() {
     try {
